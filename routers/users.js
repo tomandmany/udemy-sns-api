b54aa3f00e796ff50ab07238e45bffe2fc5fa8d8
鈴木翔也 <suzukishoya@suzukishouyanoMacBook-Air.local>
2023-10-26 00:33:23 +0900

    done

---
+++ routers/users.js
@@ -0,0 +1,49 @@
+const router = require("express").Router();
+const { PrismaClient } = require('@prisma/client');
+const isAuthenticated = require("../middlewares/isAuthenticated");
+
+const prisma = new PrismaClient();
+
+router.get('/find', isAuthenticated, async (req, res) => {
+    try {
+        const user = await prisma.user.findUnique({ where: { id: req.userId } });
+        if (!user) {
+            res.status(404).json({ error: 'ユーザーが見つかりませんでした。' });
+        }
+        res
+            .status(200)
+            .json({
+                user: { id: user.id, email: user.email, username: user.username }
+            });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+router.get('/profile/:userId', async (req, res) => {
+    const { userId } = req.params;
+
+    try {
+        const profile = await prisma.profile.findUnique({
+            where: { userId: parseInt(userId) },
+            include: {
+                user: {
+                    include: {
+                        profile: true
+                    }
+                }
+            }
+        });
+
+        if (!profile) {
+            return res.status(404).json({ message: 'プロフィールが見つかりませんでした。' });
+        }
+
+        res.status(200).json(profile);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: error.message });
+    }
+});
+
+module.exports = router;
